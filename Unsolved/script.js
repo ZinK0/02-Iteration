49d7f4f08f11805b0fb10c8ae5d2731a89b62804
Zin KO <zinko.it@outlook.com>
2024-08-29 22:46:20 +0630

    Added greeting all the student in array with for loop by using 'for of'

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -3,3 +3,9 @@
 let students = ["Mg Mg", "Aung Aung", "Hla Hla", "Mya Mya", "Kyaw Kyaw"];
 
 console.log(students.length);
+
+// Loop the array and gree with the text
+
+for (student of students) {
+  console.log(`Great to see you, ${student}`);
+}
